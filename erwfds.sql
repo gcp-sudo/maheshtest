53050b553e04583b4fc0cf0d12e8f69700b3c47d
gcp-sudo <90637991+gcp-sudo@users.noreply.github.com>
2023-07-26 14:25:24 +0530

    Update erwfds.sql

--- erwfds.sql
+++ erwfds.sql
@@ -1,1 +1,12 @@
 ZXCvzxc c zXC A
+stage('Checkout') {
+            steps {
+                // Checkout the repository from Bitbucket with credentials
+                checkout([$class: 'GitSCM',
+                          branches: [[name: '*/main']],
+                          userRemoteConfigs: [[
+                              url: 'https://bitbucket.org/your-bitbucket-user/your-repo.git',
+                              credentialsId: 'your-bitbucket-credentials-id'
+                          ]]])
+            }
+        }
